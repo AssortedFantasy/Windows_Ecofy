68b78f697ea9295ce8ea705c0deddb337ed8266b
Jehanzeb Mirza <jehanzeb@ualberta.ca>
2023-12-18 23:37:07 -0500

    Completed

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const win = @cImport({
     @cInclude("windows.h");
     @cInclude("psapi.h");
+    @cInclude("processthreadsapi.h");
     @cInclude("aclapi.h");
     @cInclude("Tlhelp32.h");
 });
@@ -12,6 +13,11 @@ const config_file_name = "ecofy.conf";
 var file_buff: [1 << 14]u8 = undefined; // 16K max
 var name_buff: [512][]const u8 = undefined; // Because we dont use linear scan.
 var hash_map_buff: [1 << 12]u8 = undefined;
+
+const EcoErrors = error{
+    WINAPI,
+    ToManyWatches,
+};
 
 pub fn main() !void {
     var fba = std.heap.FixedBufferAllocator.init(&file_buff);
@@ -28,7 +34,7 @@ pub fn main() !void {
     while (it.next()) |name| {
         if (name.len > 0) {
             log.info("Watched name: \"{s}\"", .{name});
-            if (name_count >= name_buff.len) return error.TooManyNames;
+            if (name_count >= name_buff.len) return EcoErrors.ToManyWatches;
             name_buff[name_count] = name;
             name_count += 1;
         }
@@ -39,12 +45,13 @@ pub fn main() !void {
     var hashmap_fba = std.heap.FixedBufferAllocator.init(&hash_map_buff);
     const hash_alloc = hashmap_fba.allocator();
 
-    {
-        const snapshot = win.CreateToolhelp32Snapshot(win.TH32CS_SNAPPROCESS | win.TH32CS_SNAPTHREAD, 0);
+    while (true) {
+        // const snapshot = win.CreateToolhelp32Snapshot(win.TH32CS_SNAPPROCESS | win.TH32CS_SNAPTHREAD, 0);
+        const snapshot = win.CreateToolhelp32Snapshot(win.TH32CS_SNAPPROCESS, 0);
 
         if (snapshot == win.INVALID_HANDLE_VALUE) {
             log.err("Failed to get process snapshot, error code: {}", .{win.GetLastError()});
-            return error.WINAPI;
+            return EcoErrors.WINAPI;
         }
 
         var process: win.PROCESSENTRY32 = undefined;
@@ -52,21 +59,57 @@ pub fn main() !void {
 
         if (win.Process32First(snapshot, &process) == 0) {
             log.err("Failed to copy process information from snapshot, error code: {}", .{win.GetLastError()});
-            return error.WINAPI;
+            return EcoErrors.WINAPI;
         }
 
+        // Set of processes which we want to have EcoQoSed
         var important_ids = std.AutoHashMap(win.DWORD, void).init(hash_alloc);
         defer hashmap_fba.reset();
 
         while (true) {
             const proc_name = std.mem.sliceTo(&process.szExeFile, 0);
-            log.info("Opened Process: \"{s}\", Id: {}", .{ proc_name, process.th32ProcessID });
+            log.debug("Opened Process: \"{s}\", Id: {}", .{ proc_name, process.th32ProcessID });
             //log.debug("{}", .{process});
 
             for (names) |watched_name| {
                 if (std.mem.eql(u8, watched_name, proc_name)) {
                     std.log.info("Process Id: {} matched \"{s}\"", .{ process.th32ProcessID, watched_name });
                     try important_ids.put(process.th32ProcessID, {});
+
+                    // Set to EcoQoS.
+                    const proc_handle = win.OpenProcess(win.PROCESS_SET_INFORMATION, win.FALSE, process.th32ProcessID);
+                    if (proc_handle == null) {
+                        std.log.warn("Failed to open process: {} \"{s}\", error code: {}", .{ process.th32ProcessID, proc_name, win.GetLastError() });
+                        continue;
+                    }
+
+                    var process_info: win.PROCESS_POWER_THROTTLING_STATE = .{
+                        .Version = win.PROCESS_POWER_THROTTLING_CURRENT_VERSION,
+                        .ControlMask = win.PROCESS_POWER_THROTTLING_EXECUTION_SPEED,
+                        .StateMask = win.PROCESS_POWER_THROTTLING_EXECUTION_SPEED,
+                    };
+
+                    if (win.SetProcessInformation(
+                        proc_handle,
+                        win.ProcessPowerThrottling,
+                        &process_info,
+                        @sizeOf(@TypeOf(process_info)),
+                    ) == 0) {
+                        log.err("Failed to set process information, error code: {}", .{win.GetLastError()});
+                        return EcoErrors.WINAPI;
+                    }
+
+                    if (win.SetPriorityClass(proc_handle, win.IDLE_PRIORITY_CLASS) == 0) {
+                        log.err("Failed to set idle priority class, error code: {}", .{win.GetLastError()});
+                        return EcoErrors.WINAPI;
+                    }
+
+                    log.info("Set \"{s}\" to EcoQoS", .{proc_name});
+
+                    if (win.CloseHandle(proc_handle) == 0) {
+                        std.log.err("Failed to close process handle, error code: {}", .{win.GetLastError()});
+                        return EcoErrors.WINAPI;
+                    }
                 }
             }
 
@@ -74,13 +117,71 @@ pub fn main() !void {
                 const err = win.GetLastError();
                 if (err == win.ERROR_NO_MORE_FILES) break;
                 log.err("Failed to get next process information, error code: {}", .{err});
+                return EcoErrors.WINAPI;
+            }
+        }
+
+        // We dont need thread stuff.
+        if (false) {
+            var thread: win.THREADENTRY32 = undefined;
+            thread.dwSize = @sizeOf(@TypeOf(thread));
+
+            if (win.Thread32First(snapshot, &thread) == 0) {
+                log.err("Failed to copy thread information from snapshot, error code: {}", .{win.GetLastError()});
+                return EcoErrors.WINAPI;
+            }
+
+            while (true) {
+                //log.debug("Thread: {} {}", .{ thread.th32OwnerProcessID, thread.th32ThreadID });
+                if (important_ids.contains(thread.th32OwnerProcessID)) {
+                    log.info("Thread {} owned by watched {}", .{ thread.th32ThreadID, thread.th32OwnerProcessID });
+
+                    // Set to EcoQoS
+                    const thread_handle = win.OpenThread(win.THREAD_SET_INFORMATION, win.FALSE, thread.th32ThreadID);
+                    if (thread_handle == null) {
+                        std.log.warn("Failed to open thread: {}, error code: {}", .{ thread.th32ThreadID, win.GetLastError() });
+                        continue;
+                    }
+
+                    var thread_info: win.THREAD_POWER_THROTTLING_STATE = .{
+                        .Version = win.THREAD_POWER_THROTTLING_CURRENT_VERSION,
+                        .ControlMask = win.THREAD_POWER_THROTTLING_EXECUTION_SPEED,
+                        .StateMask = win.THREAD_POWER_THROTTLING_EXECUTION_SPEED,
+                    };
+
+                    if (win.SetThreadInformation(
+                        thread_handle,
+                        win.ThreadPowerThrottling,
+                        &thread_info,
+                        @sizeOf(@TypeOf(thread_info)),
+                    ) == 0) {
+                        log.err("Failed to set thread information, error code: {}", .{win.GetLastError()});
+                        return EcoErrors.WINAPI;
+                    }
+
+                    log.info("Set \"{}\" to EcoQoS", .{thread.th32ThreadID});
+
+                    if (win.CloseHandle(thread_handle) == 0) {
+                        std.log.err("Failed to close thread handle, error code: {}", .{win.GetLastError()});
+                        return EcoErrors.WINAPI;
+                    }
+                }
+
+                if (win.Thread32Next(snapshot, &thread) == 0) {
+                    const err = win.GetLastError();
+                    if (err == win.ERROR_NO_MORE_FILES) break;
+                    log.err("Failed to get next thread information from snapshot, error code: {}", .{err});
+                    return EcoErrors.WINAPI;
+                }
             }
         }
 
         if (win.CloseHandle(snapshot) == 0) {
             log.err("Failed to close snapshot, error code: {}", .{win.GetLastError()});
-            return error.WINAPI;
+            return EcoErrors.WINAPI;
         }
+
+        win.Sleep(std.time.ms_per_min * 5);
     }
 
     // var buff16: [1 << 8]u16 = undefined;
@@ -89,7 +190,7 @@ pub fn main() !void {
     // const name_len_u16 = win.GetProcessImageFileNameW(handle, &buff16, buff16.len);
     // if (name_len_u16 == 0) {
     //     log.err("Failed to get image name, error code: {}", .{win.GetLastError()});
-    //     return error.WINAPI;
+    //     return EcoErrors.WINAPI;
     // }
 
     // const name_len_u8 = try std.unicode.utf16leToUtf8(&buff8, buff16[0..name_len_u16]);
@@ -120,7 +221,7 @@ pub fn main() !void {
     //         @sizeOf(@TypeOf(process_info)),
     //     ) == 0) {
     //         log.err("Failed to set process information, error code: {}", .{win.GetLastError()});
-    //         return error.WINAPI;
+    //         return EcoErrors.WINAPI;
     //     }
     //     log.info("Set \"{s}\" to EcoQoS", .{proc_name});
 
